40b74392118529a4f6088338fe99a1e5e3f241ad
cargorango <cargorango@gmail.com>
2026-05-06 22:30:52 +0700

    fix: mark all 182 free elective courses in DB, add missing 3 courses

--- add-courses.js
+++
@@ -1,3 +0,0 @@
-﻿const fs=require('fs');
-const existing=JSON.parse(fs.readFileSync('prisma/free-electives.json','utf8'));
-const more=[
--- gen-seed.js
+++
@@ -1,5 +0,0 @@
-﻿const fs = require('fs')
-const courses = []
-// batch will be appended
-fs.writeFileSync('prisma/seed-free-electives.ts', 'placeholder', 'utf8')
-console.log('init ok')
